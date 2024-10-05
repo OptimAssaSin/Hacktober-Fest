6d690e32fcfa88a3112c61125fa41f692edced92
AnshuXLynx <anshumandash2006pro@gmail.com>
2024-10-05 15:47:21 -0700

    46C.c

--- C/Easy/46.c
+++ C/Easy/46.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
-int add_numbers(int a, int b)  
-int result = a + b;  
+int add_numbers(int a, int b);  
+
 int main() {
 int num1 = 5;
 int num2 = 10;
 
 printf("%d\n", add_numbers(num1, num2));
 return 0;
+}
+
+int add_numbers(int a, int b){
+result = a + b;
+return result;
 }
 
